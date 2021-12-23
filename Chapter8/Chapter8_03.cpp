9b89a296a4b420db9a85428c93cbbb59a2e93b80
SeungJoonChoi <tmdwns0845@naver.com>
2021-12-23 15:57:42 +0900

    211223

---
+++ Chapter8/Chapter8_03.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Fraction
+{
+private:
+    int m_numerator;
+    int m_denominator;
+
+public:
+    Fraction(const int &num_in = 1, const int &den_in = 1)
+    {
+        m_numerator = num_in;
+        m_denominator = den_in;
+
+        cout << "Fraction() Constructor" << endl;
+    }
+    void print()
+    {
+        cout << m_numerator << " / " << m_denominator << endl;
+    }
+};
+
+int main()
+{
+
+    Fraction one_thirds = Fraction{1, 3};
+    one_thirds.print();
+
+    return 0;
+}
